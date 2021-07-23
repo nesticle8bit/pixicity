42d610644576eeaef2b4ff87895b2db0b510d7ca
Julio Poveda <jjpoveda92@gmail.com>
2021-07-23 12:41:32 -0500

    wip: componente para visualizar los posts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { SearchComponent } from './pages/search/search.component';
+import { PostsViewComponent } from './posts/posts-view/posts-view.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'registro', component: RegisterComponent },
   { path: 'buscar', component: SearchComponent },
+
+  { path: 'posts/:categoria/:id/:nombre-post', component: PostsViewComponent },
 
   { path: '*', redirectTo: '' }
 ];
