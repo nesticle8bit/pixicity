3ac66bf09938b3f462cab7ac0880a99b14478e75
Julio Poveda <jjpoveda92@gmail.com>
2021-07-28 14:34:40 -0500

    Implementando el login y guards para los jwt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,12 +31,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { NgSelectConfig, NgSelectModule } from '@ng-select/ng-select';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
 import { IHttpSecurityService } from './services/interfaces/httpSecurity.interface';
 import { HttpSecurityService } from './services/implementations/httpSecurity.service';
 import { IHttpParametrosService } from './services/interfaces/httpParametros.interface';
 import { HttpParametrosService } from './services/implementations/httpParametros.service';
+import { JwtInterceptor } from './shared/interceptors/jwt.interceptor';
 
 @NgModule({
   declarations: [
@@ -82,6 +83,8 @@ import { HttpParametrosService } from './services/implementations/httpParametros
     DisplayComponentService,
     { provide: IHttpSecurityService, useClass: HttpSecurityService },
     { provide: IHttpParametrosService, useClass: HttpParametrosService },
+
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
 })
