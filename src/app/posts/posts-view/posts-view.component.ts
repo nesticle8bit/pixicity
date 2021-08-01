1966634f48065f7fdf8e94707ba0632bd8a9b45e
Julio Poveda <jjpoveda92@gmail.com>
2021-08-01 00:12:32 -0500

    Mejorando el side del ver posts

--- src/app/posts/posts-view/posts-view.component.ts
+++ src/app/posts/posts-view/posts-view.component.ts
@@ -10,6 +10,7 @@ import { IHttpPostsService } from 'src/app/services/interfaces/httpPosts.interfa
 export class PostsViewComponent implements OnInit {
   public postId: number = 0;
   public post: any;
+  public usuario: any;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -20,13 +21,13 @@ export class PostsViewComponent implements OnInit {
     this.activatedRoute.paramMap.subscribe((values: any) => {
       this.postId = values.get('id');
 
-      this.postService.getPostById(this.postId).subscribe((post: any) => {
-        if(post) {
-          post.tags = post.etiquetas.split(',')
+      this.postService.getPostById(this.postId).subscribe((value: any) => {
+        if(value.post) {
+          value.post.tags = value.post.etiquetas.split(',')
         }
 
-        console.log(post);
-        this.post = post;
+        this.post = value.post;
+        this.usuario = value.usuario;
       });
     });
   }
