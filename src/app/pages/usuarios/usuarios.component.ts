13557018caf07cf881a92571d673c08140c484e2
Julio Poveda <jjpoveda92@gmail.com>
2021-08-07 13:44:13 -0500

    Paginación para la lista de Usuarios

--- src/app/pages/usuarios/usuarios.component.ts
+++ src/app/pages/usuarios/usuarios.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 import { IHttpParametrosService } from 'src/app/services/interfaces/httpParametros.interface';
 import { IHttpSecurityService } from 'src/app/services/interfaces/httpSecurity.interface';
+import { PaginationService } from 'src/app/services/shared/pagination.service';
 
 @Component({
   selector: 'app-usuarios',
@@ -11,18 +13,26 @@ export class UsuariosComponent implements OnInit {
   public usuarios: any = [];
   public generos: string[] = ['Hombre', 'Mujer', 'Otro', 'Todos'];
   public paises: any[] = [];
+  public totalCount: number = 0;
 
   constructor(
     private securityService: IHttpSecurityService,
-    private parametrosService: IHttpParametrosService
+    private parametrosService: IHttpParametrosService,
+    public paginationService: PaginationService
   ) { }
 
   ngOnInit(): void {
-    this.securityService.getUsuarios().subscribe((value: any) => {
-      this.usuarios = value.usuarios;
-    });
-
+    let pageEvent: PageEvent = { pageIndex: 0, pageSize: 12, length: 0 };
+    this.pageChange(pageEvent);
+    
     this.getPaises();
+  }
+
+  getUsuarios(): void {
+    this.securityService.getUsuarios().subscribe((response: any) => {
+      this.usuarios = response.usuarios;
+      this.totalCount = response.pagination.totalCount;
+    });
   }
 
   getPaises(): void {
@@ -31,4 +41,8 @@ export class UsuariosComponent implements OnInit {
     });
   }
 
+  pageChange(event: PageEvent): void {
+    this.paginationService.change(event);
+    this.getUsuarios();
+  }
 }
