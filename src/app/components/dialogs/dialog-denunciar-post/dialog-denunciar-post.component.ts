8450f44914f349b55b1d47f743cd97a375a46a06
Julio Poveda <jjpoveda92@gmail.com>
2021-08-30 20:40:21 -0500

    Llamado a backend del denunciar post

--- src/app/components/dialogs/dialog-denunciar-post/dialog-denunciar-post.component.ts
+++ src/app/components/dialogs/dialog-denunciar-post/dialog-denunciar-post.component.ts
@@ -1,5 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { IHttpPostsService } from 'src/app/services/interfaces/httpPosts.interface';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-dialog-denunciar-post',
@@ -7,6 +10,7 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./dialog-denunciar-post.component.scss']
 })
 export class DialogDenunciarPostComponent implements OnInit {
+  public formGroup: FormGroup;
   public razonDenuncia: any[] = [{
     id: 1,
     label: 'Re-post'
@@ -48,9 +52,36 @@ export class DialogDenunciarPostComponent implements OnInit {
     label: 'Otra razón (por favor especificar)'
   }];
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private formBuilder: FormBuilder,
+    private postService: IHttpPostsService,
+    private dialogRef: MatDialogRef<DialogDenunciarPostComponent>
+  ) {
+    this.formGroup = this.formBuilder.group({
+      postId: [this.data?.id, Validators.required],
+      razonDenunciaId: [undefined, Validators.required],
+      comentarios: ['', Validators.required]
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  enviarDenuncia(): void {
+    const form = Object.assign({}, this.formGroup.value);
+
+    this.postService.reportPost(form).subscribe((response: any) => {
+      if(response) {
+        Swal.fire({
+          title: 'Denunciado',
+          text: `El post ${this.data?.titulo} ha sido denunciado correctamente, el equipo de modaración revisará en la brevedad`,
+          icon: 'success',
+          timer: 3000
+        }).then(() => {
+          this.dialogRef.close(response);
+        });
+      }
+    });
+  }
 }
