035e987a6fce8793c5240630184443026b7d876c
Julio Poveda <jjpoveda92@gmail.com>
2021-09-04 18:03:34 -0500

    Paginación para los favoritos del usuario

--- src/app/pages/favoritos/favoritos.component.ts
+++ src/app/pages/favoritos/favoritos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 import { IHttpGeneralService } from 'src/app/services/interfaces/httpGeneral.interface';
+import { PaginationService } from 'src/app/services/shared/pagination.service';
 
 @Component({
   selector: 'app-favoritos',
@@ -8,16 +10,26 @@ import { IHttpGeneralService } from 'src/app/services/interfaces/httpGeneral.int
 })
 export class FavoritosComponent implements OnInit {
   public favoritos: any[] = [];
+  public totalCount: number = 0;
 
   constructor(
+    public paginationService: PaginationService,
     private httpGeneral: IHttpGeneralService
   ) { }
 
   ngOnInit(): void {
+    this.getFavoritos();
+  }
+
+  getFavoritos(): void {
     this.httpGeneral.getFavoritosByUser().subscribe((response: any) => {
       this.favoritos = response.favoritos;
-      console.log(this.favoritos);
+      this.totalCount = response.pagination.totalCount;
     });
   }
 
+  pageChange(event: PageEvent): void {
+    this.paginationService.change(event);
+    this.getFavoritos();
+  }
 }
