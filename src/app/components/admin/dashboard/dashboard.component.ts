48f6fd632d2685ef42b97973e398a6e10e841aa7
Julio Poveda <jjpoveda92@gmail.com>
2021-10-06 13:53:39 -0500

    Agregando más elementos en el menú de administración

--- src/app/components/admin/dashboard/dashboard.component.ts
+++ src/app/components/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DisplayComponentService } from 'src/app/services/shared/displayComponents.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private displayService: DisplayComponentService
+  ) { }
 
   ngOnInit(): void {
+    this.displayService.setDisplay({ mainMenu: true, footer: true, searchFooter: false});
   }
 
 }
