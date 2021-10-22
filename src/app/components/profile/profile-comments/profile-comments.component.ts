fdd5d49794568bc1cd71f85dca0ceaab67507a7b
Julio Poveda <jjpoveda92@gmail.com>
2021-10-21 20:29:33 -0500

    wip: comentarios en el perfil del usuario

---
+++ src/app/components/profile/profile-comments/profile-comments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-comments',
+  templateUrl: './profile-comments.component.html',
+  styleUrls: ['./profile-comments.component.scss']
+})
+export class ProfileCommentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
