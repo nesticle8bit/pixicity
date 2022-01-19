ea000e51026a7979498a8370b90c35529e00f27f
Julio Poveda <jjpoveda92@gmail.com>
2022-01-19 16:10:48 -0500

    Implementacion del Ir al cielo del footer

--- src/app/main/main-footer/main-footer.component.ts
+++ src/app/main/main-footer/main-footer.component.ts
@@ -1,3 +1,4 @@
+import { ViewportScroller } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { DisplayComponentModel } from 'src/app/models/shared/displayComponent.model';
 import { DisplayComponentService } from 'src/app/services/shared/displayComponents.service';
@@ -5,19 +6,25 @@ import { DisplayComponentService } from 'src/app/services/shared/displayComponen
 @Component({
   selector: 'main-footer',
   templateUrl: './main-footer.component.html',
-  styleUrls: ['./main-footer.component.scss']
+  styleUrls: ['./main-footer.component.scss'],
 })
 export class MainFooterComponent implements OnInit {
   public display!: DisplayComponentModel;
 
   constructor(
-    private displayService: DisplayComponentService
-  ) { }
+    private displayService: DisplayComponentService,
+    private viewPort: ViewportScroller
+  ) {}
 
   ngOnInit(): void {
-    this.displayService.getDisplay().subscribe((value: DisplayComponentModel) => {
-      this.display = value;
-    });
+    this.displayService
+      .getDisplay()
+      .subscribe((value: DisplayComponentModel) => {
+        this.display = value;
+      });
   }
 
+  goToHeaven(): void {
+    this.viewPort.scrollToPosition([0, 0]);
+  }
 }
