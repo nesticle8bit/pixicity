603133529d47105b76e84d2c2cba2c9eb27622fd
nesticle8bit <jjpoveda92@gmail.com>
2022-02-13 19:30:47 -0500

    Comentarios ocultos en "sinComentarios"

--- src/app/posts/posts-view/posts-view.component.ts
+++ src/app/posts/posts-view/posts-view.component.ts
@@ -8,7 +8,7 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-posts-view',
   templateUrl: './posts-view.component.html',
-  styleUrls: ['./posts-view.component.scss']
+  styleUrls: ['./posts-view.component.scss'],
 })
 export class PostsViewComponent implements OnInit {
   public currentUser: any;
@@ -21,17 +21,22 @@ export class PostsViewComponent implements OnInit {
     private postService: IHttpPostsService,
     private displayService: DisplayComponentService,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.activatedRoute.paramMap.subscribe((values: any) => {
       this.getPostById(+values.get('id'));
       this.post = {
-        titulo: values.get('nombre-post')
+        titulo: values.get('nombre-post'),
       };
     });
 
-    this.displayService.setDisplay({ mainMenu: true, footer: true, searchFooter: false, submenu: false});
+    this.displayService.setDisplay({
+      mainMenu: true,
+      footer: true,
+      searchFooter: false,
+      submenu: false,
+    });
 
     this.currentUser = this.securityService.getCurrentUser();
   }
@@ -45,7 +50,7 @@ export class PostsViewComponent implements OnInit {
 
       if (value.post) {
         value.post.url = value.post.titulo.toLowerCase().replace(/\s/g, '-');
-        value.post.tags = value.post.etiquetas.split(',')
+        value.post.tags = value.post.etiquetas.split(',');
       }
 
       this.post = value.post;
@@ -53,8 +58,6 @@ export class PostsViewComponent implements OnInit {
       this.post.id = postId;
     });
   }
-
-
 
   actualizarPost(): void {
     this.router.navigate([`posts/actualizar/${this.post.id}`]);
@@ -69,34 +72,38 @@ export class PostsViewComponent implements OnInit {
       cancelButtonText: `Cancelar`,
     }).then((result) => {
       if (result.isConfirmed) {
-        this.postService.deletePost(this.post.id).subscribe((response: boolean) => {
-          if (response) {
-            Swal.fire({
-              title: 'Eliminado',
-              text: 'El post ha sido eliminado correctamente, ahora nadie lo podrá visualizar',
-              icon: 'success',
-              timer: 3000
-            }).then(() => {
-              this.router.navigate(['']);
-            });
-          }
-        });
+        this.postService
+          .deletePost(this.post.id)
+          .subscribe((response: boolean) => {
+            if (response) {
+              Swal.fire({
+                title: 'Eliminado',
+                text: 'El post ha sido eliminado correctamente, ahora nadie lo podrá visualizar',
+                icon: 'success',
+                timer: 3000,
+              }).then(() => {
+                this.router.navigate(['']);
+              });
+            }
+          });
       }
-    })
+    });
   }
 
   quitarSticky(): void {
-    this.postService.changeStickyPost(this.post.id).subscribe((response: any) => {
-      if (response) {
-        Swal.fire({
-          title: 'Sticky',
-          text: 'Se ha cambiado el sticky para este post correctamente',
-          icon: 'success',
-          timer: 3000
-        });
+    this.postService
+      .changeStickyPost(this.post.id)
+      .subscribe((response: any) => {
+        if (response) {
+          Swal.fire({
+            title: 'Sticky',
+            text: 'Se ha cambiado el sticky para este post correctamente',
+            icon: 'success',
+            timer: 3000,
+          });
 
-        this.post.sticky = !this.post.sticky;
-      }
-    });
+          this.post.sticky = !this.post.sticky;
+        }
+      });
   }
 }
