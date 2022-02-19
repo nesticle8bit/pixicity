b6a908216ea823f6ccf9bab6246eb3387a3f7f75
nesticle8bit <jjpoveda92@gmail.com>
2022-02-18 19:17:51 -0500

    Opacidad en el buscar comentarios

--- src/app/home/home-last-comments/home-last-comments.component.ts
+++ src/app/home/home-last-comments/home-last-comments.component.ts
@@ -4,23 +4,34 @@ import { IHttpPostsService } from 'src/app/services/interfaces/httpPosts.interfa
 @Component({
   selector: 'app-home-last-comments',
   templateUrl: './home-last-comments.component.html',
-  styleUrls: ['./home-last-comments.component.scss']
+  styleUrls: ['./home-last-comments.component.scss'],
 })
 export class HomeLastCommentsComponent implements OnInit {
+  public refreshComments: boolean = false;
   public lastComments: any = [];
 
-  constructor(
-    private postService: IHttpPostsService
-  ) { }
+  constructor(private postService: IHttpPostsService) {}
 
   ngOnInit(): void {
     this.getUltimosComentarios();
+    console.log(` __________________________
+    / No lo hagas, no abras la \
+    \ consola, es peligroso 🥺 /
+     --------------------------
+             \
+              \
+               ___
+              (o o)
+             (  V  )
+            /--m-m-
+    `);
   }
 
   getUltimosComentarios(): void {
+    this.refreshComments = true;
     this.postService.getUltimosComentarios().subscribe((comentarios: any) => {
       this.lastComments = comentarios;
+      this.refreshComments = false;
     });
   }
-
 }
