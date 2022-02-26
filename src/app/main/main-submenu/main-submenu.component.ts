dd65c389ff3c5590ca9b3613c33c2a7cfea1b247
nesticle8bit <jjpoveda92@gmail.com>
2022-02-26 13:24:26 -0500

    Validaciones en las opciones

--- src/app/main/main-submenu/main-submenu.component.ts
+++ src/app/main/main-submenu/main-submenu.component.ts
@@ -1,6 +1,7 @@
 import { DisplayComponentService } from 'src/app/services/shared/displayComponents.service';
 import { DisplayComponentModel } from 'src/app/models/shared/displayComponent.model';
 import { Component, OnInit } from '@angular/core';
+import { IHttpSecurityService } from 'src/app/services/interfaces/httpSecurity.interface';
 
 @Component({
   selector: 'main-submenu',
@@ -8,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-submenu.component.scss'],
 })
 export class MainSubmenuComponent implements OnInit {
+  public currentUser: any;
   public display!: DisplayComponentModel;
 
-  constructor(private displayService: DisplayComponentService) {}
+  constructor(
+    private displayService: DisplayComponentService,
+    private securityService: IHttpSecurityService
+  ) {
+    this.currentUser = this.securityService.getCurrentUser();
+  }
 
   ngOnInit(): void {
     this.displayService
