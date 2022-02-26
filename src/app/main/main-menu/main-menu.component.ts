474ac954c6b5781aeaa66dbd1f5a86cc7b745bea
nesticle8bit <jjpoveda92@gmail.com>
2022-02-26 12:46:15 -0500

    Ocultar admin si no tiene ese rol

--- src/app/main/main-menu/main-menu.component.ts
+++ src/app/main/main-menu/main-menu.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { IHttpSecurityService } from 'src/app/services/interfaces/httpSecurity.interface';
 
 @Component({
   selector: 'main-menu',
   templateUrl: './main-menu.component.html',
-  styleUrls: ['./main-menu.component.scss']
+  styleUrls: ['./main-menu.component.scss'],
 })
 export class MainMenuComponent implements OnInit {
+  public currentUser: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private securityService: IHttpSecurityService) {
+    this.currentUser = this.securityService.getCurrentUser();
   }
 
+  ngOnInit(): void {}
 }
