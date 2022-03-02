08c3bfbe5ad34b44e2c7d9cb62a454a4c34bcd7c
nesticle8bit <jjpoveda92@gmail.com>
2022-03-01 20:11:48 -0500

    Arreglo de bugs

--- src/app/services/shared/displayComponents.service.ts
+++ src/app/services/shared/displayComponents.service.ts
@@ -1,22 +1,27 @@
-import { Injectable } from "@angular/core";
-import { Observable, Subject } from "rxjs";
-import { DisplayComponentModel } from "src/app/models/shared/displayComponent.model";
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { DisplayComponentModel } from 'src/app/models/shared/displayComponent.model';
 
 @Injectable({ providedIn: 'root' })
 export class DisplayComponentService {
-    private display = new Subject<DisplayComponentModel>();
+  private display = new Subject<DisplayComponentModel>();
 
-    constructor() { }
+  constructor() {}
 
-    setDisplay(display: DisplayComponentModel): void {
-        this.display.next(display);
-    }
+  setDisplay(display: DisplayComponentModel): void {
+    this.display.next(display);
+  }
 
-    clearDisplay(): void {
-        this.display.next();
-    }
+  clearDisplay(): void {
+    this.display.next({
+      mainMenu: false,
+      footer: false,
+      searchFooter: false,
+      submenu: false,
+    });
+  }
 
-    getDisplay(): Observable<DisplayComponentModel> {
-        return this.display.asObservable();
-    }
+  getDisplay(): Observable<DisplayComponentModel> {
+    return this.display.asObservable();
+  }
 }
