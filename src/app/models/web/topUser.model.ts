9ca873e541e3453e9aff60ef7576775fde4ef651
nesticle8bit <jjpoveda92@gmail.com>
2022-03-03 19:15:45 -0500

    Avatar en el top posts

--- src/app/models/web/topUser.model.ts
+++ src/app/models/web/topUser.model.ts
@@ -2,10 +2,12 @@ export class TopUserModel {
   id: number;
   userName: string;
   puntos: number;
+  avatar: string;
 
-  constructor(id: number, userName: string, puntos: number) {
+  constructor(id: number, userName: string, puntos: number, avatar: string) {
     this.id = id;
     this.userName = userName;
     this.puntos = puntos;
+    this.avatar = avatar;
   }
 }
