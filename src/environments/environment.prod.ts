2b1d6c13ce3cb9e62e1c0fc3bd9bf6e211af8b00
nesticle8bit <jjpoveda92@gmail.com>
2022-10-14 18:03:43 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   api: 'https://api.pixicity.io',
-  captchaKey: '6LeKGkQiAAAAAHQMTKLj2tz4RCfZ39W3ihmw5ewg'
+  captchaKey: '6LeKGkQiAAAAABws36bq2X5jY1vLOntEvZHQMbnf'
 };
