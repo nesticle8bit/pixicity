8a0dafe81f6e42840e0b4c8539b6e940924b1ce4
nesticle8bit <jjpoveda92@gmail.com>
2023-01-12 08:35:34 -0500

    Mejoras en los links del generador de posts

--- src/app/posts/posts-generator/posts-generator.component.ts
+++ src/app/posts/posts-generator/posts-generator.component.ts
@@ -33,39 +33,51 @@ export class PostsGeneratorComponent implements OnInit {
     let buildPost = ``;
 
     if (form.imagenInicial) {
-      buildPost += `<img src='https://i.imgur.com/m7UFsBF.png' alt='Bienvenidos a Pixicity!' title='Bienvenidos a Pixicity!' /><br/>`;
+      buildPost += `<img src='https://i.imgur.com/m7UFsBF.png' alt='Bienvenidos a Pixicity!' title='Bienvenidos a Pixicity!' /><br/><br/>`;
       buildPost += `<img src='${form.imagenInicial}' alt='Imagen Principal del Post' title='Imagen Principal del Post' /><br/><br/>`;
     }
 
     if (form.informacion) {
-      buildPost += `<img src='https://i.imgur.com/WDamJPN.png' alt='Información' title='Información' />`;
-      buildPost += `<p>${form.informacion.replaceAll('\n', '<br/>')}</p><br/><br/>`;
+      buildPost += `<img src='https://i.imgur.com/WDamJPN.png' alt='Información' title='Información' /><br/><br/>`;
+      buildPost += `<p>${form.informacion.replaceAll(
+        '\n',
+        '<br/>'
+      )}</p><br/><br/>`;
     }
 
     if (form.image1 || form.image2 || form.image3) {
-      buildPost += `<img src='https://i.imgur.com/7QlX8rE.png' alt='Imágenes' title='Imágenes' />`;
+      buildPost += `<img src='https://i.imgur.com/7QlX8rE.png' alt='Imágenes' title='Imágenes' /><br/><br/>`;
 
       if (form.image1) {
-        buildPost += `<img src='${form.image1}' alt='Imagen' /><br/>`;
+        buildPost += `<img src='${form.image1}' alt='Imagen' /><br/><br/>`;
       }
 
       if (form.image2) {
-        buildPost += `<img src='${form.image2}' alt='Imagen' /><br/>`;
+        buildPost += `<img src='${form.image2}' alt='Imagen' /><br/><br/>`;
       }
 
       if (form.image3) {
-        buildPost += `<img src='${form.image3}' alt='Imagen' /><br/>`;
+        buildPost += `<img src='${form.image3}' alt='Imagen' /><br/><br/>`;
       }
     }
 
     if (form.links) {
-      buildPost += `<img src='https://i.imgur.com/Vzhycg4.png' alt='Links de Descarga' title='Links de Descarga' />`;
-      buildPost += `${form.links.replaceAll('\n', '<br/>')}<br/><br/>`;
+      buildPost += `<img src='https://i.imgur.com/Vzhycg4.png' alt='Links de Descarga' title='Links de Descarga' /><br/><br/>`;
+
+      let links = form.links.split('\n').filter((link: string) => link);
+
+      if (links?.length > 0) {
+        links.map((link: string) => {
+          buildPost += `<a href='${link}'>${link}</a><br/>`;
+        });
+      }
+
+      buildPost += `<br/><br/>`;
     }
 
     if (form.password) {
-      buildPost += `<img src='https://i.imgur.com/EJuzny4.png' alt='Contraseña' title='Contraseña' />`;
-      buildPost += `🔐 <strong>Contraseña:</strong> ${form.password}<br/><br/>`;
+      buildPost += `<img src='https://i.imgur.com/EJuzny4.png' alt='Contraseña' title='Contraseña' /><br/>`;
+      buildPost += `🔐 <strong>Contraseña:</strong> ${form.password}<br/>`;
     }
 
     this.dialogRef.close(buildPost);
