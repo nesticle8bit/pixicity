8a14641422e7c9b84ccca01fcb5ed7a48b9353e2
nesticle8bit <nesticle8bit@proton.me>
2023-03-06 16:31:15 -0500

    Se movieron los componentes de carpeta

--- src/app/components/admin/control-de-contenido/paginas/table-paginas/table-paginas.component.ts
+++ src/app/components/admin/control-de-contenido/paginas/table-paginas/table-paginas.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { PaginationService } from 'src/app/services/shared/pagination.service';
 
 @Component({
   selector: 'app-table-paginas',
   templateUrl: './table-paginas.component.html',
-  styleUrls: ['./table-paginas.component.scss']
+  styleUrls: ['./table-paginas.component.scss'],
 })
 export class TablePaginasComponent implements OnInit {
+  public paginas: any[] = [];
+  public totalCount: number = 0;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    public paginationService: PaginationService) {
+    this.paginationService.change({ pageIndex: 0, pageSize: 25, length: 0 });
   }
 
+  ngOnInit(): void {}
+
+  getPaginas(): void {
+
+  }
+
+  createUpdatePagina(pagina: any = null): void {}
+
+  deletePagina(pagina: any): void {}
+
+  pageChange(event: PageEvent): void {
+    this.paginationService.change(event);
+    this.getPaginas();
+  }
 }
